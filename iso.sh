eabb31823d3b1d0507c7c7036bb788a83b7e24ac
DropDemBits <r3usrlnd@gmail.com>
2017-08-17 15:55:28 -0400

    Add Makefile to userland project

--- iso.sh
+++ iso.sh
@@ -6,13 +6,13 @@ mkdir -p ./isodir/boot/grub
 cat > ./isodir/boot/grub/grub.cfg << EOF
 menuentry "CondorFOS" {
 	multiboot /boot/condor.kern
-	module /boot/programs/test "/test.bin"
+	module /bin/test.bin "/test.bin"
 }
 EOF
 
 cp ./kernel/condor.kern ./isodir/boot/condor.kern
 
 # Copy programs
-cp -Ru userland/programs/bin/ isodir/boot/programs/
+cp -Ru userland/programs/bin/ isodir/
 
 grub-mkrescue -o condor.iso ./isodir
--- userland/programs/make-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./obj
-mkdir -p ./bin
-i686-elf-gcc -c -o ./obj/test test.S
-objcopy --only-section=.text --output-target binary ./obj/test ./bin/test
